6d30df9dd49a53e8969eaf168c0b7793f104b78d
Hei <waityou32505@163.com>
2023-03-24 11:20:21 +0800

    3.24学习

---
+++ DataType3/列表堆栈.py
@@ -0,0 +1,7 @@
+stack_list = [1, 2, 3, 4]
+
+# 当没有指定pop()参数时，就退出列表的最后一个值，也就是栈顶
+stack_list.pop()
+stack_list.pop()
+
+print(stack_list)
---
+++ 模块/读取模块.py
@@ -0,0 +1,6 @@
+import sys
+
+print("参数")
+for a in sys.argv:
+    print(a)
+print('\n\nPython 路径为：', sys.path, '\n')
